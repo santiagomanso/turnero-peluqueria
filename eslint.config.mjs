42312aaa62f408c931154d91b78a0f30215678c4
santiagomanso <santiagomanso.dev@gmail.com>
2026-02-02 19:24:18 -0300

    fix: linter / prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,11 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      quotes: ["error", "double", { avoidEscape: true }],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
