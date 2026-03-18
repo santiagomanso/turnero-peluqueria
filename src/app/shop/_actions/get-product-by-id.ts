fcf7440aa49b38565bc3d0e30aa15cbe3fab3bbd
Santi <santiagomanso.dev@gmail.com>
2026-03-18 15:36:53 -0300

    feat(shop): add get-product-by-id server action

---
+++ src/app/shop/_actions/get-product-by-id.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { getProductById } from "@/services/shop";
+import type { Product } from "@/types/shop";
+
+export async function getProductByIdAction(id: string): Promise<Product | null> {
+  return await getProductById(id);
+}
