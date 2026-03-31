2c86bd513291863bfa108bc9fb3dde5a6000e89e
Santi <santiagomanso.dev@gmail.com>
2026-03-30 22:09:34 -0300

    revert: remove page transition changes (template.tsx, PageTransition, layout changes)

--- src/app/template.tsx
+++
@@ -1,7 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div style={{ animation: "page-enter 0.55s cubic-bezier(0.16,1,0.3,1) forwards" }}>
-      {children}
-    </div>
-  );
-}
