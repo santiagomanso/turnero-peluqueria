4f22d506b5d151fa0ec490044f0dc9e58dd8002d
santiagomanso <santiagomanso.dev@gmail.com>
2026-04-13 03:23:19 -0300

    feat: replace caged layout with full-width shell in category page

--- src/app/shop/[category]/page.tsx
+++ src/app/shop/[category]/page.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from "react";
 import { notFound } from "next/navigation";
-import { Container } from "@/components/ui/container";
-import Navbar from "@/components/navbar";
-import CartButton from "@/app/shop/_components/cart-button";
+import ShopNavbar from "@/app/shop/_components/shop-navbar";
+import ShopSidebar from "@/app/shop/_components/shop-sidebar";
+import ShopCategoryPills from "@/app/shop/_components/shop-category-pills";
 import CategoryData from "./_components/category-data";
 import CategorySkeleton from "./_components/category-skeleton";
 import { CategoryHeader } from "./_components/category-view";
@@ -19,23 +19,28 @@ export default async function CategoryPage({ params }: Props) {
   if (!category) notFound();
 
   return (
-    <Container.wrapper className="h-svh">
-      <Container.content className="h-svh md:h-[85vh]">
-        {/* Fixed: navbar + category header + switcher */}
-        <Navbar
-          title="Tienda Online"
-          hideSettings
-          rightElement={<CartButton />}
-        />
-        <CategoryHeader category={category} />
+    <div className="min-h-svh flex flex-col bg-surface dark:bg-zinc-950 font-archivo">
+      <ShopNavbar />
 
-        {/* CategoryView handles its own internal scroll */}
-        <div className="flex-1 min-h-0 flex flex-col -mx-4 px-4">
-          <Suspense fallback={<CategorySkeleton />}>
-            <CategoryData category={category} />
-          </Suspense>
+      <div className="flex flex-1 min-h-0">
+        <ShopSidebar />
+
+        {/* Main content */}
+        <div className="flex-1 flex flex-col min-h-0">
+          {/* Mobile/tablet pills */}
+          <div className="pt-5">
+            <ShopCategoryPills />
+          </div>
+
+          {/* Scrollable area */}
+          <div className="flex-1 overflow-y-auto px-5 pb-8 lg:px-12 lg:py-10">
+            <CategoryHeader category={category} />
+            <Suspense fallback={<CategorySkeleton />}>
+              <CategoryData category={category} />
+            </Suspense>
+          </div>
         </div>
-      </Container.content>
-    </Container.wrapper>
+      </div>
+    </div>
   );
 }
